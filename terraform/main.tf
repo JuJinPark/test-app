32fbe6a7d911ece43801adea33e692e8775305f5
JuJinPark <wnwls1216@gmail.com>
2025-04-11 22:43:38 +0900

    feat: terraform change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "3.0.1-rc6"
+    }
+  }
+}
+
 provider "proxmox" {
   pm_api_url      = var.pm_api_url
   pm_user         = var.pm_user
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,6 @@
 variable "pm_api_url" { type = string }
 variable "pm_user"    { type = string }
-variable "pm_password" { type = string, sensitive = true }
+variable "pm_password" {
+  type = string
+  sensitive = true
+}
