d7c618471b4bd275896b3c88e96dfb33e7cce9b5
JuJinPark <wnwls1216@gmail.com>
2025-04-11 20:59:51 +0900

    feat: terraform

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "proxmox" {
+  pm_api_url      = var.pm_api_url
+  pm_user         = var.pm_user
+  pm_password     = var.pm_password
+  pm_tls_insecure = true
+}
+
+resource "proxmox_lxc" "spring_app" {
+  hostname     = "spring-app"
+  target_node  = "pve"
+  ostemplate   = "local:vztmpl/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
+  password     = "changeme"
+  cores        = 2
+  memory       = 2048
+  swap         = 1024
+  rootfs       = "local-lvm:4"
+  unprivileged = true
+  start        = true
+  net0         = "name=eth0,bridge=vmbr0,ip=dhcp"
+  features {
+    nesting = true
+  }
+  cicustom = "user=local:snippets/cloud-init.yaml"
+}
