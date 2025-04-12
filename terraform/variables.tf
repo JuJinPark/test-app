0fd5ceb9b8beee7562450fd354f1f7159465acab
JuJinPark <wnwls1216@gmail.com>
2025-04-12 11:44:34 +0900

    feat: terraform change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,6 @@
 variable "pm_api_url" { type = string }
 variable "pm_user"    { type = string }
+variable "pm_token_id"    { type = string }
 variable "pm_password" {
   type = string
   sensitive = true
